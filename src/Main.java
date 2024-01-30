22c19527be262cc1bb1bc200bb031081dbe6785e
Alexei Smirnov <smirnov.mgo@yandex.ru>
2024-01-30 17:54:37 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 13_676;
+        int oneMiles = 20;
+        int miles;
+        miles = ticketPrice / oneMiles;
+        System.out.println(miles);
+
+    }
+}
+
