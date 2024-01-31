b04d9f03a666d77f816579a3892129e63e1aafd0
Alexei Smirnov <smirnov.mgo@yandex.ru>
2024-01-31 18:58:11 +0300

    Object commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
 
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 13_676;
-        int oneMiles = 20;
-        int miles;
-        miles = ticketPrice / oneMiles;
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
         System.out.println(miles);
-
     }
 }
 
+
+
+//public class Main {
+//    public static void main(String[] args) {
+//        int ticketPrice = 13_676;
+//        int oneMiles = 20;
+//        int miles;
+//        miles = ticketPrice / oneMiles;
+//        System.out.println(miles);
+//
+//    }
+//}
